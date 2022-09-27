6785ddcfead55b65ef413c68a1b6abe45430d9d1
ss <2730320860@qq.com>
2022-09-27 12:55:58 +0800

    初始化更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 function App() {
-  
-
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
