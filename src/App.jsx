88c92cc7ff2a95930e2ee7657af28d1e1555e15f
ss <2730320860@qq.com>
2022-09-27 13:16:30 +0800

    three初始应用

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Box } from '@react-three/drei'
+import * as THREE from 'three'
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <div className="h-[100vh] bg-black">
+      <Canvas className="h-full">
+        <OrbitControls />
+        <primitive object={new THREE.AxesHelper(10)} />
+        <primitive object={new THREE.GridHelper(100, 100)} />
+        <ambientLight intensity={3.5} />
+        <Box args={[2, 2, 2]}></Box>
+      </Canvas>
+    </div>
+  )
 }
 
 export default App
